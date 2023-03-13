ca514b467e57a0e55902bcbc512f9f1bfa217687
akhilakalla <akhilaanima@gmail.com>
2023-03-13 18:03:03 +0530

    added sum_num

---
+++ sum_num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	int n,r,sum=0;
+	printf("enter any number\n");
+	scanf("%d",&n);
+
+	while(n>0)
+	{
+		r=n%10;
+		sum=sum+r;
+		n=n/10;
+	}
+	printf("sum of numbers:%d\n",sum);
+}
+
